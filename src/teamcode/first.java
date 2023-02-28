da10b6afb389c31d43a368f4d3554be99d9b32cd
Yeonu <dusdn8060@naver.com>
2023-02-28 17:21:14 +0900

    Change

--- src/teamcode/first.java
+++ src/teamcode/first.java
@@ -1,10 +1,10 @@
+package teamcode;
 
-public class Hello
+public class first
 {
 
 	public static void main(String[] args)
 	{
-		System.out.println("안녕하세요");
 		// TODO Auto-generated method stub
 
 	}
